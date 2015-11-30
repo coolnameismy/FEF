e956262112bc394cdfbdab79039801ad01252239
coolnameismy <coolnameismy@hotmail.com>
2015-12-01 01:46:37 +0800

    m

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@
 
 /**
  * 组件安装
- * npm install gulp-util gulp-imagemin gulp-ruby-sass gulp-minify-css gulp-jshint gulp-uglify gulp-rename gulp-concat gulp-clean gulp-livereload tiny-lr --save-dev
+ * npm install gulp-autoprefixer gulp-util gulp-sourcemaps gulp-imagemin gulp-ruby-sass gulp-minify-css gulp-jshint gulp-uglify gulp-rename gulp-concat gulp-clean gulp-livereload tiny-lr --save-dev
  */
 
 // 引入 gulp及组件
 var gulp    = require('gulp'),                 //基础库
+    autoprefixer = require('gulp-autoprefixer') //浏览器前缀
+    handlebars = require('gulp-handlebars')
     imagemin = require('gulp-imagemin'),       //图片压缩
     sass = require('gulp-ruby-sass'),          //sass
     minifycss = require('gulp-minify-css'),    //css压缩
@@ -33,9 +35,10 @@ gulp.task('css', function () {
         cssDst = 'dist/css';
 
     sass(cssSrc)
-        // gulp.src(cssSrc).pipe(sass())
+       // gulp.src(cssSrc).pipe(sass())
         // .pipe(sass({ style: 'expanded'}))
-        .pipe(sourcemaps.write())
+        // .pipe(sourcemaps.write())
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(gulp.dest(cssDst))
         .pipe(rename({ suffix: '.min' }))
         .pipe(minifycss())
